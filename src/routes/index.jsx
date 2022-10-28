ad8a46ecca7c92c9f6f33ae13b400ee706df5530
Volodymyr Ukr <ukraincev.vova@gmail.com>
2022-10-28 22:41:07 +0300

    feat: add Routes template

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-// import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Layout from "../pages/Layout";
+import { SaleBanner } from "../pages/components";
 
 const Routing = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<SaleBanner />}></Route>
+        {/* <Route path="sale" element={<ProductPage query={gql`query} title="sale" /> */}
+        {/* <Route path="sale" element={<div>test</div>} /> */}
+        {/* <Route path="jeans" element={<ProductPage query={gql`query} title="jeans" /> */}
+        {/* <Route path="jackets" element={<ProductPage query={gql`query} title="jackets" /> */}
+        {/* <Route path="shirts" element={<ProductPage query={gql`query} title="shirts" /> */}
+      </Route>
+      {/* <Route path='*' element={<NotFoundPage />}/> */}
+    </Routes>
+  );
 };
 
 export default Routing;
