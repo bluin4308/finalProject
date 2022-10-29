5291d7858e375dcad1b9d4b5abceb5e02b4c4218
Volodymyr Ukr <ukraincev.vova@gmail.com>
2022-10-29 21:32:53 +0300

    feat: add NavLinks to all main pages

--- src/pages/components/Navigation/index.jsx
+++ src/pages/components/Navigation/index.jsx
@@ -3,10 +3,66 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className="basis-1/5 text-center font-montserrat text-3xl font-bold uppercase tracking-widest">
+    <div className="basis-1/5 text-center font-montserrat">
       <NavLink to="/">
-        <h1 className="pt-4">Logo</h1>
+        <h1 className="pt-4 text-3xl font-bold uppercase tracking-widest">
+          Logo
+        </h1>
       </NavLink>
+      <div className="mt-8 flex flex-col">
+        <NavLink
+          to="all"
+          className={
+            "my-2 transition hover:font-bold " +
+            (({ isActive }) => (isActive ? "font-bold" : " "))
+          }
+        >
+          All clothes
+        </NavLink>
+        <NavLink
+          to="jeans"
+          className={
+            "my-2 transition hover:font-bold " +
+            (({ isActive }) => (isActive ? "font-bold" : " "))
+          }
+        >
+          Jeans
+        </NavLink>
+        <NavLink
+          to="jackets"
+          className={
+            "my-2 transition hover:font-bold " +
+            (({ isActive }) => (isActive ? "font-bold" : " "))
+          }
+        >
+          Jackets
+        </NavLink>
+        <NavLink
+          to="sale"
+          className={
+            "my-2 transition hover:font-bold " +
+            (({ isActive }) => (isActive ? "font-bold" : " "))
+          }
+        >
+          Sale
+        </NavLink>
+        <NavLink
+          to="new"
+          className={
+            "my-4 transition hover:font-bold " +
+            (({ isActive }) => (isActive ? "font-bold" : " "))
+          }
+        >
+          New
+        </NavLink>
+        <NavLink
+          to=""
+          className={
+            "my-4 transition hover:font-bold " +
+            (({ isActive }) => (isActive ? "font-bold" : " "))
+          }
+        ></NavLink>
+      </div>
     </div>
   );
 };
