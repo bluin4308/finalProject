b64359c594f3b7f6e1ce5fae804911b91551abcf
Volodymyr Ukr <ukraincev.vova@gmail.com>
2022-10-30 12:36:12 +0200

    feat: change NavLink from to=sale => to=new

--- src/pages/components/SaleBanner/index.jsx
+++ src/pages/components/SaleBanner/index.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect } from "react";
 import { NavLink, useOutletContext } from "react-router-dom";
+
 const SaleBanner = () => {
-  const [setTitle] = useOutletContext();
+  const [title, setTitle] = useOutletContext();
+
   useEffect(() => {
-    setTitle("Sales");
+    setTitle("New arrivals");
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [title]);
+
   return (
-    <div className="relative">
+    <div className="relative mt-1">
       <img
         className="h-auto w-2/3"
         src="https://dsa1mc.wp4.host/wp-content/uploads/2022/10/jeans.jpg"
         alt="Banner"
       />
       <div className="text-section absolute top-0 px-2 py-4 font-normal">
-        <p className="text-3xl">New arrivals</p>
+        {/* <p className="text-3xl">New arrivals</p> */}
         <p className="text-xl uppercase">collection 2022</p>
         <NavLink
-          to="sale"
-          className="hover: mt-2 block w-max bg-black p-2 text-white"
+          to="new"
+          className="mt-2 block w-max bg-black p-2 text-white transition hover:bg-white hover:text-black"
         >
           shop now
         </NavLink>
