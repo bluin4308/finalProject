3acffe8c95e0342461feec7452215a0577f48add
Volodymyr Ukr <ukraincev.vova@gmail.com>
2022-10-30 11:52:09 +0200

    feat: add Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,83 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Layout } from "../pages";
+import { Layout, ProductPage, NotFoundPage } from "../pages";
 import { SaleBanner } from "../pages/components";
+import { GET_ITEMS } from "../apollo";
+
+// const HOC = (Comp) => {
+//   return (props) => {
+//     <Comp {...props} />;
+//   };
+// };
+
+// const Sale = HOC(ProductPage);
+
+const Sale = (props) => {
+  return <ProductPage {...props} />;
+};
+
+const Jeans = (props) => {
+  return <ProductPage {...props} />;
+};
+
+const AllClothes = (props) => {
+  return <ProductPage {...props} />;
+};
+
+const Jackets = (props) => {
+  return <ProductPage {...props} />;
+};
+
+const New = (props) => {
+  return <ProductPage {...props} />;
+};
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<SaleBanner />}></Route>
-        {/* <Route path="sale" element={<ProductPage query={gql`query} title="sale" /> */}
-        {/* <Route path="jeans" element={<ProductPage query={gql`query} title="jeans" /> */}
-        {/* <Route path="jackets" element={<ProductPage query={gql`query} title="jackets" /> */}
-        {/* <Route path="shirts" element={<ProductPage query={gql`query} title="shirts" /> */}
+        <Route path="/" element={<SaleBanner />} />
+        <Route
+          path="sale"
+          element={
+            <Sale title="Sale" query={GET_ITEMS} tag={{ tag: ["sale"] }} />
+          }
+        />
+        <Route
+          path="jeans"
+          element={
+            <Jeans title="Jeans" query={GET_ITEMS} tag={{ tag: ["jeans"] }} />
+          }
+        />
+        <Route
+          path="all"
+          element={
+            <AllClothes
+              title="All clothes"
+              query={GET_ITEMS}
+              tag={{ tag: [""] }}
+            />
+          }
+        />
+        <Route
+          path="jackets"
+          element={
+            <Jackets
+              title="Jackets"
+              query={GET_ITEMS}
+              tag={{ tag: ["jacket"] }}
+            />
+          }
+        />
+        <Route
+          path="new"
+          element={
+            <New title="New clothes" query={GET_ITEMS} tag={{ tag: ["new"] }} />
+          }
+        />
+
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
-      {/* <Route path='*' element={<NotFoundPage />}/> */}
     </Routes>
   );
 };
