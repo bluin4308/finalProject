ef1d2d4ce5b0f3f483284cf6d942cdeebba1d16d
Volodymyr Ukr <ukraincev.vova@gmail.com>
2022-10-30 11:52:45 +0200

    fix: fix ProductPage import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import Layout from "./Layout";
+import ProductPage from "./ProductPage";
+import NotFoundPage from "./NotFoundPage";
 
-export { Layout };
+export { Layout, ProductPage, NotFoundPage };
